d6c05ccb0fe3aff2ce6ffedd9557892443bed3de
Valentine <valentinesamuel2580@gmail.com>
2020-12-07 16:30:45 +0100

    made the responsiveness better on the hero section because it got broke

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroComponent } from './hero/hero.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [AppComponent, HeroComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+  ],
+  exports: [MatIconModule],
   providers: [],
   bootstrap: [AppComponent],
 })
