607591d47418fcb73a736bb7860b38fdca2c9af0
Valentine <valentinesamuel2580@gmail.com>
2020-12-09 22:31:15 +0100

    added custom event binding and started tracking my progress on trello today

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-hero',
@@ -9,4 +9,12 @@ export class HeroComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
+
+  @Output() touch = new EventEmitter();
+
+  onTouch() {
+    this.touch.emit(
+      alert('This is direct from the hero to know how i can pass data')
+    );
+  }
 }
