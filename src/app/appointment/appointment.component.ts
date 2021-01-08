2ee07096e0dcf7c2690ce11f840ba06017f6edbe
Valentine <valentinesamuel2580@gmail.com>
2021-01-09 00:17:52 +0100

    (FEAT):Added the deactivate logic in the appointment file

--- src/app/appointment/appointment.component.ts
+++ src/app/appointment/appointment.component.ts
@@ -1,13 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { IDeactivateGuard } from '../shared/deactivate-appointment-guard.service';
 
 @Component({
   selector: 'app-appointment',
   templateUrl: './appointment.component.html',
   styleUrls: ['./appointment.component.css'],
 })
-export class AppointmentComponent implements OnInit {
+export class AppointmentComponent implements OnInit, IDeactivateGuard {
   appointmentDetails: {
+    email: string;
+    department: string;
+    password: string;
+    time: string;
+  };
+
+  copiedAppointmentDetails: {
     email: string;
     department: string;
     password: string;
@@ -24,7 +32,18 @@ export class AppointmentComponent implements OnInit {
         department: params['dept'],
         time: params['time'],
       };
+      this.copiedAppointmentDetails = { ...this.appointmentDetails };
     });
   }
   //sett
+  canLeave() {
+    console.log(this.copiedAppointmentDetails);
+    if (confirm('Are you sure you want to go back?')) {
+      if (confirm('All unsaved changes will be lost')) {
+        return true;
+      }
+    } else {
+      return false;
+    }
+  }
 }
